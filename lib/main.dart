94cf60ba815b6bd9690a2745889d2e33965967b7
Manojit Chakraborty <jeetmanojit98@gmail.com>
2022-04-08 21:21:29 +0530

    theme extracted

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: avoid_unnecessary_containers
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/pages/home_page.dart';
 import 'package:flutter_demo/pages/login_page.dart';
 import 'package:flutter_demo/utils/routes.dart';
+import 'package:flutter_demo/widgets/themes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,9 +15,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(
-          brightness: Brightness.light, primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(brightness: Brightness.dark),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
