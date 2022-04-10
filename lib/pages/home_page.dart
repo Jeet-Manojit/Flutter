35b06b5145f2500c3e12bb9932d96d613a9fa781
Manojit Chakraborty <jeetmanojit98@gmail.com>
2022-04-10 17:09:38 +0530

    Day 13 completed, json data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_demo/models/catalog.dart';
 import 'package:flutter_demo/pages/login_page.dart';
 import 'package:flutter_demo/widgets/drawer.dart';
 import 'package:flutter_demo/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   String name = "";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+  }
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatelogModel.items[0]);
@@ -19,7 +39,7 @@ class HomePage extends StatelessWidget {
         child: ListView.builder(
           itemCount: dummyList.length,
           itemBuilder: (context, index) {
-            return ItemWidget(item: dummyList[index]);
+            return ItemWidget(item: dummyList[index++]);
           },
         ),
       ),
