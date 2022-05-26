cf63dd5ed9e4277fe13770ea1f9a05a5fa536b3b
Manojit Chakraborty <jeetmanojit98@gmail.com>
2022-05-27 02:29:26 +0530

    day 17 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:flutter_demo/models/catalog.dart';
 import 'package:flutter_demo/widgets/drawer.dart';
 import 'package:flutter_demo/widgets/themes.dart';
-//import 'package:velocity_x/velocity_x.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -36,22 +36,112 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: MyTheme.creamColor,
         body: SafeArea(
-      child: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: const [
-            Text(
-              "Catalog App",
-              style: TextStyle(
-                fontWeight: FontWeight.w700,
-                letterSpacing: 1,
-                fontSize: 40,
-              ),
-            )
-          ],
+          child: Container(
+            padding: Vx.m32,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                CatelogHeader(),
+                if (CatelogModel.items != null && CatelogModel.items.isNotEmpty)
+                  CatelogList().expand()
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class CatelogHeader extends StatelessWidget {
+  const CatelogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+        "Trending App".text.xl2.make(),
+      ],
+    );
+  }
+}
+
+class CatelogList extends StatelessWidget {
+  const CatelogList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: CatelogModel.items.length,
+        itemBuilder: (context, index) {
+          final catelog = CatelogModel.items[index];
+          return CatelogItem(catelog: catelog);
+        });
+  }
+}
+
+class CatelogItem extends StatelessWidget {
+  final Item catelog;
+
+  const CatelogItem({super.key, required this.catelog})
+      : assert(catelog != null);
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+        child: Row(
+      children: [
+        CatelogImage(
+          image: catelog.image,
         ),
-      ),
-    ));
+        Expanded(
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            catelog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+            catelog.desc.text.textStyle(context.captionStyle).make(),
+            10.heightBox,
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceBetween,
+              buttonPadding: Vx.m1,
+              children: [
+                "\$${catelog.price}".text.xl.bold.make(),
+                ElevatedButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(MyTheme.darkBluishColor),
+                        shape: MaterialStateProperty.all(StadiumBorder())),
+                    child: "Buy".text.make())
+              ],
+            ).pOnly(right: 8.0)
+          ],
+        ))
+      ],
+    )).white.rounded.square(150).make().py16();
+  }
+}
+
+class CatelogImage extends StatelessWidget {
+  final String image;
+
+  const CatelogImage({super.key, required this.image}) : assert(image != null);
+  @override
+  Widget build(BuildContext context) {
+    return Image.network(image)
+        .box
+        .p8
+        .rounded
+        .color(MyTheme.creamColor)
+        .make()
+        .p16()
+        .w40(context);
   }
 }
--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
-        primarySwatch: Colors.deepPurple,
-        appBarTheme: AppBarTheme(
-            color: Colors.white,
-            elevation: 0,
-            iconTheme: IconThemeData(color: Colors.black),
-            toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
-            titleTextStyle: Theme.of(context).textTheme.headline6),
-      );
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+        toolbarTextStyle: Theme.of(context).textTheme.bodyText2,
+        titleTextStyle: Theme.of(context).textTheme.headline6,
+      ));
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
